84361a58f86bf8926561f2a052ccc503d53027c8
Danyl Brahan <danylbragan@gmail.com>
2023-11-20 09:38:38 +0100

    dsfsd

--- src/App.jsx
+++ src/App.jsx
@@ -24,52 +24,48 @@ function App() {
     }
   }
 
-  // useEffect(() => {
-  //   fetch(BASE_URL)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       // console.log(data);
-  //       if (data && data.rates) {          
-  //         const firstCurrency = Object.keys(data.rates)[0]
-  //         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-  //         setFromCurrency(data.base)
-  //         setToCurrency(firstCurrency)
-  //         setExchangeRate(data.rates[firstCurrency])
-  //       }
-  //     })
-  // }, [])
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        if (data && data.rates) {          
+          const firstCurrency = Object.keys(data.rates)[0]
+          setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+          setFromCurrency(data.base)
+          setToCurrency(firstCurrency)
+          setExchangeRate(data.rates[firstCurrency])
+        }
+      })
+  }, [])
 
-  // useEffect(() => {
-  //   if (fromCurrency != null && toCurrency != null) {
-  //     fetch(`${BASE_URL}`)
-  //     .then(res => {
-  //       if (!res.ok) {
-  //         throw new Error(`HTTP error! Status: ${res.status}`);
-  //       }
-  //       return res.json();
-  //     })
-  //       .then(data => setExchangeRate(data.rates[toCurrency]))
-  //       .catch(error => console.error("Помилка при отриманні курсу обміну:", error));
-  //   }
-  // }, [fromCurrency, toCurrency])
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${BASE_URL}`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+        .then(data => setExchangeRate(data.rates[toCurrency]))
+        .catch(error => console.error("Помилка при отриманні курсу обміну:", error));
+    }
+  }, [fromCurrency, toCurrency])
 
-  // function handleFromAmountChange(e) {
-  //   setAmount(e.target.value)
-  //   setAmountInFromCurrency(true)
-  // }
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
 
-  // function handleToAmountChange(e) {
-  //   setAmount(e.target.value)
-  //   setAmountInFromCurrency(false)
-  // }
-
-  // function handleSwap() {
-  //   setFromCurrency(toCurrency);
-  //   setToCurrency(fromCurrency);
-  // }
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <>
+      <img className='currency_image' src='https://play-lh.googleusercontent.com/pOiddrf8_IPWiUOyhj1cxbz2CzWejYdFjs8H5qGfIxnQs8YMNqZr4ScRevMhVnjS7Sg' alt='currency'></img>
       <h1 className='main_title'>Currency converter</h1>
       <div className='main_container'>
         <div className='container'>
@@ -82,7 +78,7 @@ function App() {
                 currencyOptions={currencyOptions}
                 selectedCurrency={fromCurrency}
                 onChangeCurrency={e => setFromCurrency(e.target.value)}
-                // onChangeAmount={handleFromAmountChange}
+                onChangeAmount={handleFromAmountChange}
                 amount={fromAmount}
               />
             </div>
@@ -94,12 +90,11 @@ function App() {
               currencyOptions={currencyOptions}
               selectedCurrency={toCurrency}
               onChangeCurrency={e => setToCurrency(e.target.value)}
-              // onChangeAmount={handleToAmountChange}
+              onChangeAmount={handleToAmountChange}
               amount={toAmount}
               />
             </div>
           </div>
-            {/* <button onClick={handleSwap}><img src="./components/images/swap_horiz_FILL0_wght400_GRAD0_opsz24.png" alt="" /></button> */}
         </div>
       </div>
     </>
